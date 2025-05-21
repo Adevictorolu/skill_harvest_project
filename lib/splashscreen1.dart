67c441e4d1bbc38f552580879ee5e60bd4563248
Ademola Victor Oluokun <137506855+Adevictorolu@users.noreply.github.com>
2025-05-21 14:41:25 +0100

    Added Now

--- lib/splashscreen1.dart
+++ lib/splashscreen1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
 
 class SplashScreen1 extends StatelessWidget {
   const SplashScreen1({super.key});
@@ -8,15 +9,78 @@ class SplashScreen1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SvgPicture.asset(
-              height: 150,
-              width: double.infinity,
-              'asset/img/open-an-account-svgrepo-com.svg',
-            ),
-          ],
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                height: 300,
+                width: double.infinity,
+                'asset/img/check-in-svgrepo-com.svg',
+              ),
+              // SvgPicture.asset(
+              //   height: 150,
+              //   width: double.infinity,
+              //   'asset/img/inancial-security-svgrepo-com.svg',
+              // ),
+              Text(
+                'Create you own\nstudy plan',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+                textAlign: TextAlign.center,
+              ),
+              Gap(2),
+              Text(
+                'Study according to the\nstudy plan, make study\nmore motivated',
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 13),
+                textAlign: TextAlign.center,
+              ),
+              Gap(100),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      backgroundColor: Colors.blue,
+                      fixedSize: Size(170, 40),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  Gap(20),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(width: 1, color: Colors.blue),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      backgroundColor: Colors.white,
+                      fixedSize: Size(170, 40),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Log in',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
