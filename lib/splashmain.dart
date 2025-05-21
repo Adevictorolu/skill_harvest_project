4da057fad260780f11edbfd8c3a63d01599ef676
Ademola Victor Oluokun <137506855+Adevictorolu@users.noreply.github.com>
2025-05-21 09:07:30 +0100

    Added Recently

---
+++ lib/splashmain.dart
@@ -0,0 +1,58 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+
+class SplashMain extends StatefulWidget {
+  const SplashMain({super.key});
+
+  @override
+  State<SplashMain> createState() => _SplashMainState();
+}
+
+class _SplashMainState extends State<SplashMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              transform: GradientRotation(10),
+              colors: [Colors.blue, Color.fromARGB(255, 10, 60, 117)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomLeft,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AnimatedTextKit(
+                animatedTexts: [
+                  WavyAnimatedText(
+                    'Avotek-to-Learn',
+                    textStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                ],
+                pause: Duration(seconds: 1),
+                // isRepeatingAnimation: true,
+              ),
+              Text(
+                'Learning Your Way',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
